369af18d626fa501903669e6c82ee75154c1e379
songyz2019 <songyz2023dlut@outlook.com>
2025-09-13 10:29:57 +0800

    update for publishing in PyPI

--- script/dist.sh
+++ script/dist.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 mkdir -p dist
-zip -r dist/dist.zip . -x ".venv/*" -x ".git/*" -x "script/*" -x "dist/*" -x "**/__pycache__/*"
+zip -r dist/hsi-preprocessing-toolkit-win64.zip . -x ".venv/*" -x ".git/*" -x "script/*" -x "dist/*" -x "**/__pycache__/*"
